bff999c6605bf9af6b0da44c9a4726d132db6595
Nedja <nedjapejasinovic@gmail.com>
2016-02-28 19:47:52 +0100

    Dynamic image workspaces tabs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,9 +21,121 @@ namespace NPGui
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<TabItem> _tabItems;
+        private TabItem _tabAdd;
+
         public MainWindow()
         {
-            InitializeComponent();
+            try
+            {
+                InitializeComponent();
+
+                // initialize tabItem array
+                _tabItems = new List<TabItem>();
+
+                // add a tabItem with + in header 
+                TabItem tabAdd = new TabItem();
+                tabAdd.Header = "+";
+
+                _tabItems.Add(tabAdd);
+
+                // add first tab
+                this.AddTabItem();
+
+                // bind tab control
+                tabDynamic.DataContext = _tabItems;
+
+                tabDynamic.SelectedIndex = 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
+        private TabItem AddTabItem()
+        {
+            int count = _tabItems.Count;
+
+            // create new tab item
+            TabItem tab = new TabItem();
+            tab.Header = string.Format("Tab {0}", count);
+            tab.Name = string.Format("tab{0}", count);
+            tab.HeaderTemplate = tabDynamic.FindResource("TabHeader") as DataTemplate;
+
+            // add controls to tab item, this case I added just a textbox
+            var txt = new NPGui.Controls.ImageWorkspaceControl();// new TextBox();
+            txt.Name = "txt";
+            tab.Content = txt;
+
+            // insert tab item right before the last (+) tab item
+            _tabItems.Insert(count - 1, tab);
+            return tab;
+        }
+
+        private void tabDynamic_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            TabItem tab = tabDynamic.SelectedItem as TabItem;
+
+            if (tab != null && tab.Header != null)
+            {
+                if (tab.Header.Equals("+"))
+                {
+                    // clear tab control binding
+                    tabDynamic.DataContext = null;
+
+                    // add new tab
+                    TabItem newTab = this.AddTabItem();
+
+                    // bind tab control
+                    tabDynamic.DataContext = _tabItems;
+
+                    // select newly added tab item
+                    tabDynamic.SelectedItem = newTab;
+                }
+                else
+                {
+                    // your code here...
+                }
+            }
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            string tabName = (sender as Button).CommandParameter.ToString();
+
+            var item = tabDynamic.Items.Cast<TabItem>().Where(i => i.Name.Equals(tabName)).SingleOrDefault();
+
+            TabItem tab = item as TabItem;
+
+            if (tab != null)
+            {
+                if (_tabItems.Count < 3)
+                {
+                    MessageBox.Show("Cannot remove last tab.");
+                }
+                else if (MessageBox.Show(string.Format("Are you sure you want to remove the tab '{0}'?", tab.Header.ToString()),
+                    "Remove Tab", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+                    // get selected tab
+                    TabItem selectedTab = tabDynamic.SelectedItem as TabItem;
+
+                    // clear tab control binding
+                    tabDynamic.DataContext = null;
+
+                    _tabItems.Remove(tab);
+
+                    // bind tab control
+                    tabDynamic.DataContext = _tabItems;
+
+                    // select previously selected tab. if that is removed then select first tab
+                    if (selectedTab == null || selectedTab.Equals(tab))
+                    {
+                        selectedTab = _tabItems[0];
+                    }
+                    tabDynamic.SelectedItem = selectedTab;
+                }
+            }
+        }
+
     }
 }
